f480fedcdeb6b3d4ffe472e938e78c9d968a428a
n4zla <nazla.kayla05@gmail.com>
2025-05-28 14:07:21 +0700

    Memperbaiki tabel dan seeder user

--- database/migrations/2025_05_14_030734_create_users_table.php
+++ database/migrations/2025_05_14_030734_create_users_table.php
@@ -1,24 +1,26 @@
 <?php
-     use Illuminate\Database\Migrations\Migration;
-     use Illuminate\Database\Schema\Blueprint;
-     use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-     return new class extends Migration
-     {
-         public function up(): void
-         {
-             Schema::create('users', function (Blueprint $table) {
-                 $table->id();
-                 $table->string('name');
-                 $table->string('email')->unique();
-                 $table->string('password');
-                 $table->enum('role', ['admin', 'user', 'kurir', 'tenant'])->default('user');
-                 $table->timestamps();
-             });
-         }
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password')->nullable();
+            $table->string('firebase_uid')->nullable()->unique();
+            $table->enum('role', ['admin', 'user', 'kurir', 'tenant'])->default('user');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
 
-         public function down(): void
-         {
-             Schema::dropIfExists('users');
-         }
-     };
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
